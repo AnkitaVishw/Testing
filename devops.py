ac6191d4b6c09a465b0eff471febe4043a88064b
AnkitaVishw <ankushrma1999@gmail.com>
2026-09-03 08:11:23 +0530

    Devopes tool added

---
+++ devops.py
@@ -0,0 +1,13 @@
+print("Continuous Integration & Continuous Deployment (CI/CD)")
+print("Git")
+print("Github")
+print("CircleCI")
+
+print("Containerization & Orchestration")
+print("Docker")
+print("Kubernetes")
+print("Docker Swarm")
+
+print(" Infrastructure as Code (IaC) & Configuration Management")
+print("Terraform")
+print("Ansible")
